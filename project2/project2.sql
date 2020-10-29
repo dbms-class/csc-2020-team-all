413c3f3c7d96502a8307d21c9dc982fff52d1db8
troechka <albinapt1u@gmail.com>
2020-10-29 20:37:09 +0300

    Added sql request.

---
+++ project2/project2.sql
@@ -0,0 +1,123 @@
+-- строка представляет собой объект действующее вещество с его свойствами
+CREATE TABLE ActiveComponent(
+Name TEXT PRIMARY KEY,
+--чтобы каждая формула соответствовала одному химическому веществу
+Formula TEXT UNIQUE
+ );
+
+-- строка представляет собой объект Лаборатория с его свойствами
+CREATE TABLE Laboratory(
+Name TEXT PRIMARY KEY,
+Head_name TEXT
+-- других ключей не предусмотрено
+ );
+
+-- строка представляет собой объект сертификат с его свойствами
+CREATE TABLE Certificate(
+Number BIGINT PRIMARY KEY,
+Validity DATE,
+--N:1 лаборатория выдает много уникальных сертификатов 
+LaboratoryName TEXT REFERENCES Laboratory (Name)
+-- других ключей не предусмотрено
+ );
+
+
+-- строка представляет собой объект Лекарство с его свойствами
+CREATE TABLE Drug(
+Id SERIAL PRIMARY KEY,
+-- торговое имя уникально
+Trade_name TEXT UNIQUE NOT NULL,
+-- интернациональное имя уникально
+International_name TEXT UNIQUE NOT NULL,
+Form TEXT CHECK(Form IN ('таблетка', 'капсула', 'ампула')),
+Maker TEXT,
+-- N:1 каждое действующее вещество может соответствовать нескольким лекарствам
+ActiveComponentName TEXT REFERENCES ActiveComponent (Name),
+-- 1:1 у каждого лекарства индивидуальный сертификат
+CertificateNumber BIGINT REFERENCES Certificate (Number)
+ );
+
+-- строка описывает объект Дистрибьютор и его свойства
+CREATE TABLE Distributor(
+Id SERIAL PRIMARY KEY,
+Address TEXT,
+-- мы считаем, что банковский счет и телефон уникальны для каждого дистрибьютора
+AccNumber BIGINT UNIQUE,
+ContactName TEXT,
+Telephone BIGINT UNIQUE
+ );
+
+-- строка представляет собой склад с номером и адресом
+CREATE TABLE Stock(
+Number INT PRIMARY KEY,
+Address TEXT
+-- других ключей не предусмотрено
+ );
+
+-- строка представляет собой аптеку с номером и адресом
+CREATE TABLE Pharmacy(
+Number INT PRIMARY KEY,
+Address TEXT
+-- других ключей не предусмотрено
+ );
+
+-- строка представляет собой поставку с номером, временем прибытия и получателем
+CREATE TABLE Supply(
+Number INT PRIMARY KEY,
+-- N:1 у дистрибьютора много поставок.
+DistributorId INT REFERENCES Distributor (Id), 
+-- N:1 на каждый склад может идти много поставок. 
+StockNumber INT REFERENCES Stock(Number),
+ArrivalTime DATE,
+EmployeeName TEXT
+-- других ключей не предусмотрено
+ );
+
+-- строка в таблице означает, что лекарство Drug_id присутствует в поставке SupplyNumber в размере Quantity упаковок по PackSize штук и было приобретено за DistributorPrice, суммарный вес Weight 
+CREATE TABLE SupplyDrug(
+-- связь M:N в каждой поставке много лекарств. Каждое лекарство в многих поставках
+SupplyNumber INT REFERENCES Supply(Number),
+Drug_id INT REFERENCES Drug(Id),
+Quantity INT CHECK (Quantity > 0),
+PackSize INT CHECK (PackSize > 0),
+Weight NUMERIC CHECK (Weight > 0),
+DistributorPrice INT CHECK (DistributorPrice > 0),
+-- чтобы в одной поставке не могло быть одного и того  же лекарства несколько раз
+PRIMARY KEY(SupplyNumber, Drug_id)
+-- других ключей не предусмотрено
+ );
+
+
+
+-- строка - информация о том, что в аптеке с номером PharmacyNumber есть PacksLeft штук лекарства Drug_id по цене Price 
+CREATE TABLE Prices(
+-- связь M:N. в каждой аптеке много лекарств. Каждое лекарство во многих аптеках
+PharmacyNumber INT REFERENCES Pharmacy(Number),
+Drug_id INT REFERENCES Drug(Id),
+Price INT CHECK (Price > 0),
+PacksLeft INT CHECK (PacksLeft >= 0),
+-- чтобы в одной аптеке не было нескольких записей о
+PRIMARY KEY(PharmacyNumber, Drug_id)
+-- других ключей не предусмотрено
+ );
+
+--строка представляет собой автомобиль с номером и датой последнего техобслуживания
+CREATE TABLE Auto(
+Number INT PRIMARY KEY,
+DateOfTI DATE
+-- других ключей не предусмотрено
+ );
+
+--строка представляет собой задание на поставку. Можно ездить несколько раз в день в ту же аптеку на той же машине с того же склада с тем же лекарством.
+CREATE TABLE Tasks(
+Id SERIAL PRIMARY KEY,
+-- в этой таблице связь многие ко многим между всеми внешними ключами 
+AutoNumber INT REFERENCES Auto(Number),
+PharmacyNumber INT REFERENCES Pharmacy(Number),
+Date DATE,
+StockNumber INT REFERENCES Stock(Number),
+Drug_id INT REFERENCES Drug(Id),
+PacksN INT CHECK (PacksN > 0)
+-- других ключей не предусмотрено
+ );
+
