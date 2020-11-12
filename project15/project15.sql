51c8dbf0f8166a7e557c20973889c6efa130cdf3
matrosovm <2831891@gmail.com>
2020-11-12 18:06:44 +0300

    all comments corrected without consistency of arrival_time

--- project15/project15.sql
+++ project15/project15.sql
@@ -10,26 +10,27 @@ CREATE TABLE model (
     capacity INT CHECK(capacity > 0)
 );
 
---Транспортные средства:
---модель транспортного средства с уникальным номером-идентификатором, --определенного года выпуска и его актуальным состоянием.
---ТС(модель тс(id_vehicle), бортовой номер-идентификатор, год выпуска, состояние тс)
 
-CREATE TYPE vehicle_state AS ENUM ('исправен', 'некритические неисправности', 'требует
-ремонта');
+--Транспортные средства:
+--модель транспортного средства с уникальным номером-идентификатором, определенного года выпуска и его актуальным состоянием.
+--ТС(бортовой номер-идентификатор, год выпуска, состояние тс, модель тс(id_vehicle))
+
+CREATE TYPE vehicle_state AS ENUM ('исправен', 'некритические неисправности', 'требует ремонта');
 
 CREATE TABLE vehicle (
     -- 1:M одному тс соответствует одна модель, одной модели - много тс
     id SERIAL PRIMARY KEY,
-    date_release INT NOT NULL CHECK(date_release BETWEEN 1800 AND 2200),
+    year_release INT NOT NULL CHECK(year_release BETWEEN 1800 AND 2200),
     state vehicle_state NOT NULL,
     model_id INT  NOT NULL,
     FOREIGN KEY(model_id)
         REFERENCES model(id)
 );
 
+
 --Остановки и маршруты:
 --Уникальный номер остановки, с точным адресом и количеством платформ
---Остановки(номер остановки, адрес, количетсво платформ)
+--Остановки(номер остановки, адрес, количество платформ)
 
  CREATE TABLE stop (
     id  SERIAL PRIMARY KEY,
@@ -37,11 +38,12 @@ CREATE TABLE vehicle (
     platforms_count INT CHECK(platforms_count > 0)
 );
 
+
 --Номер маршрута известный пассажирам, начало и конец маршрута.
---Маршрут(уникальный номер, тип ТС который обслуживает, начальная остановка, --конечная остановка)
+--Маршрут(уникальный номер, тип ТС который обслуживает, начальная остановка, конечная остановка)
 
 CREATE TABLE route (
--- Связь 1:M у одного маршрута одна конечная остановка, у одной конечной    --остановки много маршрутов
+-- Связь 1:M у одного маршрута одна конечная остановка, у одной конечной остановки много маршрутов
     id INT PRIMARY KEY,
     type model_type NOT NULL,
     start_id INT ,
@@ -52,40 +54,41 @@ CREATE TABLE route (
         REFERENCES stop(id)
 );
 
---Расписание(тс, номер маршрута, время прибытия, номер остановки, платформа, --выходной или рабочий день) (то есть время до минуты уникальное)
---Номер ТС который выйдет на “номер маршрута” и время прибытия на определенную --платформу в выходной или рабочий день.
+
+--Номер ТС который выйдет на “номер маршрута” и время прибытия на определенную платформу в выходной или рабочий день.
+--Расписание(тс, номер маршрута, день недели, время прибытия, номер остановки, платформа, выходной или рабочий день) (то есть время до минуты уникальное)
 
 CREATE TABLE time_table (
-    vehicle_id INT,
-    route_id INT,
-    stop_id INT,
-    platform_number INT CHECK(platform_number > 0) ,
-    time TIMESTAMP NOT NULL,
+    vehicle_id INT NOT NULL,
+    route_id INT NOT NULL,
+    is_weekend BOOl NOT NULL,
+    stop_id INT NOT NULL,
+    platform_number INT CHECK(platform_number > 0) NOT NULL,
+    arrival_time TIMESTAMP NOT NULL,
     FOREIGN KEY(stop_id)
         REFERENCES stop(id),
     FOREIGN KEY(route_id)
         REFERENCES route(id),
     FOREIGN KEY(vehicle_id)
         REFERENCES vehicle(id),
-    UNIQUE(stop_id, platform_number, time)
+    UNIQUE(stop_id, platform_number, arrival_time)
 -- На одной остановке у одной платформы в одно время может стоять одно тс
 );
 
 
---Выпуск ТС на маршруты:
 --Водитель с данным "номером служебного удостоверения" имеет данные "ФИО"
 --Водитель(номер служебного удостоверения, фамилия, имя, отчество)
 
 CREATE TABLE driver (
-    Id SERIAL PRIMARY KEY,
-    Surname TEXT NOT NULL,
-    Name TEXT NOT NULL,
-    Patronymic TEXT
+    id SERIAL PRIMARY KEY,
+    surname TEXT NOT NULL,
+    name TEXT NOT NULL,
+    patronymic TEXT
 );
 
 
---Наряд с данным "ид наряда" дан ТС с "номером-идентификатором ТС" в данную --"дату" с остановки с данным "персональным номером остановки"
---Наряд(ид наряда, ид маршрута, номер-идентификатор ТС, дата, персональный --номер остановки)
+--Наряд с данным "ид наряда" дан ТС с "номером-идентификатором ТС" в данную "дату" с остановки с данным "персональным номером остановки"
+--Наряд(ид наряда, ид маршрута, номер-идентификатор ТС, ид остановки, день, время, ид водителя)
 
 CREATE TABLE work_order (
     -- 1:M одному наряду соответствует 1 маршрут, одному маршрут много нарядов
@@ -97,7 +100,7 @@ CREATE TABLE work_order (
     vehicle_id INT,
     stop_id INT,
     day DATE NOT NULL,
-    time TIMESTAMP NOT NULL,
+    start_time TIMESTAMP NOT NULL,
     driver_id INT,
     FOREIGN KEY(route_id)
         REFERENCES route(id),
@@ -111,9 +114,8 @@ CREATE TABLE work_order (
 );
 
 
---"Фактическое время прибытия" и “предполагаемое время прибытия”, когда --водителем с данным “ид водителя” в наряде с данным "ид наряда" была посещена --остановка с данным "персональным номером остановки"
---Диспетчерская(ид наряда, ид водителя, персональный номер остановки, --фактическое время прибытия, предполагаемое время прибытия)
-
+--"Фактическое время прибытия" и “предполагаемое время прибытия”, когда водителем с данным “ид водителя” в наряде с данным "ид наряда" была посещена остановка с данным "персональным номером остановки"
+--Диспетчерская(ид наряда, ид остановки, фактическое время прибытия, предполагаемое время прибытия)
 
 CREATE TABLE control (
 -- N:M одному наряду соответствует много остановок, одной остановке - много нарядов
@@ -127,6 +129,7 @@ CREATE TABLE control (
         REFERENCES stop(id)
 );
 
+
 --Пункт билетного меню с данным "ид" и "названием"
 --БилетноеМеню(ид, название, стоимость)
 
@@ -137,15 +140,13 @@ CREATE TABLE ticket_menu (
 );
 
 
---"Время", когда была осуществлена оплата с "ид пункта билетного меню" в ТС с --"номером-идентификатором ТС"
---СтатистикаОплаты(ид пункта билетного меню, номер-идентификатор ТС, время)
+--СтатистикаОплаты(ид пункта билетного меню, день, количество валидаций билета)
 
 CREATE TABLE statistic (
-    vehicle_id INT,
-    ticket_menu_id INT,
-    time TIMESTAMP NOT NULL,
-    FOREIGN KEY(vehicle_id)
-        REFERENCES vehicle(id),
+    ticket_menu_id INT NOT NULL,
+    day DATE NOT NULL,
+    validations_count INT CHECK (validations_count >= 0),
     FOREIGN KEY(ticket_menu_id)
-        REFERENCES ticket_menu(id)
+        REFERENCES ticket_menu(id),
+    UNIQUE (ticket_menu_id, day)
 );
