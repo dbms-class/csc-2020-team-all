839523e4a2db5640604dbd2e03cf3b6697904c64
Silvery-Moon <55141575+Silvery-Moon@users.noreply.github.com>
2020-11-12 21:26:30 +0300

    Update project4.sql
    Внесены все изменения

--- project4/project4.sql
+++ project4/project4.sql
@@ -1,58 +1,127 @@
 -- Строка представляет из себя объект жанр
-CREATE TABLE Genre(name TEXT, PRIMARY KEY (name) -- все жанры уникальны
+CREATE TABLE Genre(name TEXT PRIMARY KEY -- все жанры уникальны
 );
 
 -- Строка представляет из себя объект формат
-CREATE TABLE Format(name TEXT, PRIMARY KEY (name) -- все форматы уникальны
+CREATE TABLE Format(name TEXT PRIMARY KEY -- все форматы уникальны
 );
 
--- Альбом с конкретным названием был записан  группой group_id в жанре genre_id имеет формат format_id в конкретную дату
-CREATE TABLE Album(
--- связь N:1 между альбомом и жанрами, альбомом и форматами. Конкретный альбом имеет конкретный жанр и формат. Разные альбомы могут иметь одинаковые жанры и форматы
-id INT, work_name text, group_id INT, genre_name TEXT, format_name TEXT, size INT, date DATE, CHECK (size > 0),  PRIMARY KEY (id), -- id альбома,
-FOREIGN KEY(genre_name) REFERENCES Genre(name), FOREIGN KEY(format_name) REFERENCES Format(name));
+
+-- Таблица для стран
+CREATE TABLE Country(
+	id SERIAL PRIMARY KEY, 
+	name TEXT NOT NULL UNIQUE
+);
+
+-- Таблица для регионов
+CREATE TABLE Region(
+	id SERIAL PRIMARY KEY, 
+	name TEXT, 
+	country_id INT REFERENCES Country(id)
+	UNIQUE (name, country_id), -- в одной стране не бывает двух регионов с одинаковым названием.
+);
+
+-- Таблица для стран
+CREATE TABLE Country(
+	id SERIAL PRIMARY KEY, 
+	name TEXT NOT NULL UNIQUE
+);
+
+-- Таблица для регионов
+CREATE TABLE Region(
+	id SERIAL PRIMARY KEY, 
+	name TEXT, 
+	country_id INT REFERENCES Country(id)
+	UNIQUE (name, country_id), -- в одной стране не бывает двух регионов с одинаковым названием.
+);
 
 -- Строка представляет из себя объект группа с его свойствами
-CREATE TABLE Groups(name TEXT, county TEXT, city TEXT, PRIMARY KEY (name) -- группы не могут иметь одинаковые названия, они не NULL, и мы хотим использовать этот ключ для связи M:N
+CREATE TABLE Groups(
+	id SERIAL PRIMARY KEY, -- id группы, хотим использовать этот ключ для связи M:N,
+	name TEXT NOT NULL, 
+	county_id INT REFERENCES Country(id), 
+	city_id INT REFERENCES Region(id)
+);
+
+-- Альбом с конкретным названием был записан группой group_id в жанре genre_id имеет формат format_id в конкретную дату
+CREATE TABLE Album(
+-- связь N:1 между альбомом и жанрами, альбомом и форматами. Конкретный альбом имеет конкретный жанр и формат. Разные альбомы могут иметь одинаковые жанры и форматы
+	id SERIAL PRIMARY KEY, -- id альбома,
+	work_name TEXT NOT NULL, 
+	group_id INT REFERENCES Groups(id), 
+	genre_name TEXT REFERENCES Genre(name), 
+	format_name TEXT REFERENCES Format(name), 
+	year_of_album_creation YEAR,  
 );
 
 -- Строка представляет из себя объект музыкант с его свойствами
-CREATE TABLE Musician(name TEXT, date DATE, PRIMARY KEY (name)  -- музыканты не могут иметь одинаковые имена, они не NULL, и мы хотим использовать этот ключ для связи M:N
+CREATE TABLE Musician(
+	id SERIAL PRIMARY KEY, -- id музыканта, хотим использовать этот ключ для связи M:N,
+	name TEXT NOT NULL, 
+	year_of_birth YEAR
 );
 
--- Строка представляет из себя объект музыкант с его свойствами
+-- Строка представляет из себя объект трек с его свойствами
 CREATE TABLE Track(
 --связь N:1 между треками и альбомом. Конкретный трек принадлежит одному альбому. Альбом может состоять из нескольких треков.
-id INT, name TEXT, album_id INT, size INT,
-CHECK(size >= 0), PRIMARY KEY (id), -- id трека,
-FOREIGN KEY(album_id) REFERENCES Album(id));
-
--- Строка представляет из себя объект сотрудник с его свойствами
-CREATE TABLE Employee (id INT, name TEXT, mail TEXT, PRIMARY KEY (id) -- id сотрудника
+	id SERIAL PRIMARY KEY, -- id трека,
+	name TEXT, 
+	album_id INT REFERENCES Album(id), 
+	length_in_seconds INT CHECK(size > 0),
 );
 
--- Таблица для хранения информации об участии музыканта в треке. Музыкант с “musician_name” участвовал  в песне “track_id” и имел конкретную роль.
+-- Строка представляет из себя объект сотрудник с его свойствами.
+CREATE TABLE Employee(
+ 	id SERIAL PRIMARY KEY, -- id сотрудника,
+	name TEXT NOT NULL,                   
+	mail TEXT NOT NULL UNIQUE
+);
+
+-- Таблица для возможных ролей музыкантов.
+CREATE TABLE Role(
+	id SERIAL PRIMARY KEY, -- id роли,
+	role_name TEXT NOT NULL UNIQUE
+);
+
+-- Таблица для хранения информации об участии музыканта в треке. Музыкант с “musician_id” участвовал в песне “track_id” и имел роль "role_id".
 CREATE TABLE Musician_Track(
--- связь M:N между музыкантами и треками. Каждый музыкант может записывать разные треки. В создании одного трека может участвовать несколько музыкантов, пара (музыкант, трек) не обязана быть уникальной, т.к. Музыкант может выполнять несколько ролей в записи одного трека
-musician_name TEXT, track_id INT, role TEXT, FOREIGN KEY(track_id) REFERENCES Track(id), FOREIGN KEY(musician_name) REFERENCES Musician(name));
+-- связь M:N между музыкантами и треками. Каждый музыкант может записывать разные треки, а создании одного трека может участвовать несколько музыкантов.
+-- пара (музыкант, трек, роль) обязана быть уникальной, т.к. музыкант не может выполнять одну и ту же роль в записи одного трека 
+	musician_id INT REFERENCES Musician(id), 
+	track_id INT REFERENCES Track(id), 
+	role_id INT REFERENCES Role(id),
+	UNIQUE (musician_id, track_id, role)
+);
 
 -- Таблица для хранения истории существования музыканта в группе. Музыкант с ид “musician_nameмузыкант_ид” состоял/состоит в группе с ид “ group_nameгруппа_ид” с “дата начала” по “дата окончания”
 CREATE TABLE Musician_Group(
 -- связь M:N между музыкантами и группами. В одной группе может быть несколько музыкантов и каждый музыкант может играть в нескольких группах. Пара вида (музыкант, группа) может быть не уникальной, поскольку музыкант мог уходить из группы и вернуться.
-musician_name TEXT, group_name TEXT, begin_date DATE NOT NULL, end_date DATE, FOREIGN KEY(musician_name) REFERENCES Musician(name), FOREIGN KEY(group_name) REFERENCES Groups(name));
-
+	musician_id INT REFERENCES Musician(id), 
+	group_id INT REFERENCES Groups(id), 
+	begin_date DATE NOT NULL, 
+	end_date DATE,
+	CHECK(begin_date <= end_date)
+);
+                             
 -- Таблица для хранения информации об участии сотрудника в альбоме. Сотрудник с ид “employee_id сотрудник_ид” участвовал в альбоме “ album_idальбом_ид” в качестве “роль”.
 CREATE TABLE Employee_Album(
 -- связь K:N между сотрудниками и альбома. Один сотрудник может выполнять одну роль в разных альбомах. В создании альбома могут участвовать несколько сотрудников.
-employee_id INT, album_id INT, role TEXT, FOREIGN KEY(employee_id) REFERENCES Employee(id), FOREIGN KEY(album_id) REFERENCES Album(id));
+-- Один сотрудник может принимать только одно участие в альбоме, поэтому эта пара уникальна
+	employee_id INT REFERENCES Employee(id), 
+	album_id INT REFERENCES Album(id),
+	UNIQUE (employee_id, album_id)
+);
 
 -- Создаем тип девайсы
 CREATE TYPE Device AS ENUM('Компакт-диск', 'Флешкарточка', 'Онлайн', 'Виниловая пластинка');
 
--- Строка представляет из себя объект регион
-CREATE TABLE Region(id INT, name TEXT, parent_id INT, PRIMARY KEY (id) --регион имеет конкретный id
-);
-
--- Альбом с ид “album_idальбом_ид” распространен в конкретном “стране” и  “регионе” “region_id” на “носителе” из списка носителей с локальным названием “локализованное название”
+-- Альбом с ид “album_id” распространен в конкретном “регионе” “region_id” на “носителе” из списка носителей с локальным названием “локализованное название”
 CREATE TABLE Album_production(
-	id INT, album_name TEXT, album_id INT, region_id INT, device Device, FOREIGN KEY(album_id) REFERENCES Album(id), FOREIGN KEY(region_id) REFERENCES Region(id));
+	album_id INT REFERENCES Album(id), 
+	adaptive_name_of_album TEXT NOT NULL,
+	country_id INT REFERENCES Country(id),
+	region_id INT REFERENCES Region(id), 
+	device Device, 
+	UNIQUE (album_id, adaptive_name_of_album, region_id, device)
+	CHECK(((adaptive_name_of_album IS NULL) OR (region_id IS NULL)) AND ((adaptive_name_of_album IS NOT NULL) OR (region_id IS NOT NULL)))
+);
