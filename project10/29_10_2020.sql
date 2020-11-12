d62ad28ec017c796f57971f73de5175f7fb2b34b
repl.it user <replituser@example.com>
2020-11-12 16:34:06 +0000

    review fixes

--- project10/29_10_2020.sql
+++ project10/29_10_2020.sql
@@ -1,45 +1,45 @@
--- ТС в парке с ID типа model_name выпущенного в release_year в состоянии CONDITION
-CREATE TABLE vehicle (
-	id INT NOT NULL UNIQUE, -- мы не допускаем повторяющихся идентификаторов
-	release_year INT CHECK(release_year > 1800),
-	condition TEXT CHECK(condition IN ('исправен', 'некритические неисправности', 'требует ремонта')),
-	model_name TEXT NOT NULL
+-- Тип ТС (автобус, троллейбус, трамвай, ТУАХ, электробус)
+CREATE TABLE vehicle_type (
+	NAME TEXT NOT NULL UNIQUE -- у нас нету одинаковых типов ТС
 );
 
 -- Модель ТС с названием model_name типа vehicle_type и вместимостью capacity
 CREATE TABLE vehicle_model (
 	model_name TEXT NOT NULL UNIQUE, -- у нас не повторяются названия моделей
-	vehicle_type TEXT NOT NULL,
-	capacity INT CHECK(capacity > 0)
+	vehicle_type TEXT NOT NULL REFERENCES vehicle_type(name),
+	capacity INT NOT NULL CHECK(capacity > 0)
 );
 
--- Тип ТС (автобус, троллейбус, трамвай, ТУАХ, электробус)
-CREATE TABLE vehicle_type (
-	NAME TEXT NOT NULL UNIQUE -- у нас нету одинаковых типов ТС
+-- ТС в парке с ID типа model_name выпущенного в release_year в состоянии CONDITION
+CREATE TABLE vehicle (
+	id INT NOT NULL UNIQUE, -- мы не допускаем повторяющихся идентификаторов
+	release_year INT CHECK(release_year > 1800),
+	condition TEXT CHECK(condition IN ('исправен', 'некритические неисправности', 'требует ремонта')),
+	model_name TEXT NOT NULL REFERENCES vehicle_model(model_name)
 );
 
 -- Остановка с номером ID по адресу address с количество платформ available_platforms
 CREATE TABLE stop (
 	id INT NOT NULL UNIQUE, -- у нас не повторяются остановки
 	address TEXT NOT NULL,
-	available_platforms INT CHECK(available_platforms > 0)
+	available_platforms INT NOT NULL CHECK(available_platforms > 0)
 );
 
 -- Маршрут между начальной initial_stop остановкой и final_stop остановкой с номером ID для типа ТС vehicle_type
 CREATE TABLE route (
 	id INT NOT NULL UNIQUE, -- у нас не повторяются маршруты
-	vehicle_type TEXT NOT NULL,
-	initial_stop INT NOT NULL,
-	final_stop INT NOT NULL
+	vehicle_type TEXT NOT NULL REFERENCES vehicle_type(name),
+	initial_stop INT NOT NULL REFERENCES stop(id),
+	final_stop INT NOT NULL REFERENCES stop(id)
 );
 
 -- Запись с номером ID о времени прибытия TIME номера маршрута route_id на остановку stop_id 
 -- платформу platform_number в is_weekend день недели
 CREATE TABLE timetable (
-  	ID INT NOT NULL UNIQUE, -- номер записи не повторяется
+  ID INT NOT NULL UNIQUE, -- номер записи не повторяется
 	time time NOT NULL,
-	route_id INT NOT NULL,
-	stop_id INT NOT NULL,
+	route_id INT NOT NULL REFERENCES route(id),
+	stop_id INT NOT NULL REFERENCES stop(id),
 	platform_number INT NOT NULL,
 	is_weekend boolean
 );
@@ -55,12 +55,13 @@ CREATE TABLE driver (
 -- с начальной остановки initial_stop с времени начала initial_time в день DATE
 CREATE TABLE assignment (
 	id int NOT NULL UNIQUE, -- номера нарядов не повторяются
-	vehicle_id int NOT NULL,
-	route_id int NOT NULL,
-	initial_stop int NOT NULL,
+	vehicle_id int NOT NULL REFERENCES vehicle(id),
+	route_id int NOT NULL REFERENCES route(id),
+	initial_stop int NOT NULL REFERENCES stop(id),
 	initial_time time NOT NULL,
-	driver_id int NOT NULL,
-	date date NOT NULL
+	driver_id int NOT NULL REFERENCES driver(id),
+	date date NOT NULL,
+  UNIQUE(vehicle_id, route_id, driver_id, date)
 );
 
 
@@ -68,22 +69,21 @@ CREATE TABLE assignment (
 -- наряда assignment_id на остановку stop_id
 CREATE TABLE supervisor_info (
 	id int NOT NULL UNIQUE, -- номера записей не повторяются
-	assignment_id int NOT NULL,
-	stop_id int NOT NULL,
+	assignment_id int NOT NULL REFERENCES assignment(id),
 	actual_time time NOT NULL,
-	expected_time time NOT NULL
+  timetable_id INT NOT NULL REFERENCES timetable(id)
 );
 
 -- Билет с типом TYPE стоимостью price
 CREATE TABLE ticket (
 	type TEXT NOT NULL UNIQUE, -- типы билетов уникальны
-	price int NOT NULL CHECK(price > 0)
+	price REAL NOT NULL CHECK(price > 0)
 );
 
 -- Количество валидаций number_of_validations билета типа ticket_type в наряде assignment_id
 CREATE TABLE statistics (
-  	id INT NOT NULL UNIQUE, -- номер записи о статистике не повторяется
-	assignment_id int NOT NULL,
-	ticket_type TEXT NOT NULL,
-	number_of_validations int NOT NULL CHECK(number_of_validations > 0)
+	assignment_id int NOT NULL REFERENCES assignment(id),
+	ticket_type TEXT NOT NULL REFERENCES ticket(type),
+	number_of_validations int NOT NULL CHECK(number_of_validations > 0),
+  UNIQUE(assignment_id, ticket_type)
 );
