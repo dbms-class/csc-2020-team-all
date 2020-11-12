0322c56315207c71682d23d2b1657f9fad4b0cda
Ilya Sokov <tdr.leet@gmail.com>
2020-11-12 22:05:38 +0300

    First task completed

--- project16/project16.sql
+++ project16/project16.sql
@@ -9,7 +9,7 @@ CREATE TABLE Objects(id SERIAL PRIMARY KEY,
                      name TEXT UNIQUE, -- клички не всегда есть и не могут пересекаться
                     FOREIGN KEY(purpose) REFERENCES FunctionPurposes(purpose)); -- (N: 1) с таблицей FunctionPurposes
 
--- таблица справочник стран 
+-- 
 CREATE TABLE Countries(id SERIAL PRIMARY KEY,
                       name TEXT UNIQUE NOT NULL);
 
@@ -50,12 +50,12 @@ CREATE TABLE Task(ID SERIAL PRIMARY KEY,
 -- страну country, откуда он приехал (отсюда и делегация), номер объекта object_id, где он живёт, идентификатор волонтёра volunteer_id, к которому он прикреплён.
 CREATE TABLE Sportsmens(id SERIAL PRIMARY KEY,
                         name TEXT NOT NULL,
-                       	gender TEXT CHECK(gender='m' or gender='f') DEFAULT NULL,
+                       	gender TEXT CHECK(gender='m' or gender='f') NOT NULL,
                         height INT CHECK(height > 0),
                         weight INT CHECK(weight > 0),
                         age INT CHECK(age > 0),
                         country_id INT NOT NULL,
-                        object_id INT DEFAULT NULL,
+                        object_id INT NOT NULL,
                         volunteer_id INT NOT NULL, 
                         FOREIGN key(country_id) REFERENCES Countries(id),  -- (N: 1) с таблицей Delegations
                        FOREIGN KEY(object_id) REFERENCES  Objects(ID), -- (N: 1) с таблицей Objects
---
+++ project16/useful.sql
@@ -0,0 +1,3 @@
+INSERT INTO Volunteers (name, phone) VALUES('Ricardo Milos', '+88005553535');
+INSERT INTO Countries (name) VALUES('United States');
+INSERT INTO Countries (name) VALUES('Brazil');
