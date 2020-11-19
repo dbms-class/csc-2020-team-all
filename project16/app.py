668502426517689c2caf258e773254f56ea3ad4d
Alise999 <alise.bloom@mail.ru>
2020-11-19 17:29:09 +0000

    2020.11.19

---
+++ project16/app.py
@@ -0,0 +1,120 @@
+# encoding: UTF-8
+
+## Веб сервер
+import json
+import cherrypy
+import cherrypy_cors
+
+from connect import parse_cmd_line
+from connect import create_connection
+from connect import connection_factory
+
+from static import index
+
+@cherrypy.expose
+class App(object):
+    def __init__(self, args):
+        self.args = args
+
+    @cherrypy.expose
+    def start(self):
+        return "Hello web app"
+
+    @cherrypy.expose
+    def index(self):
+      return index()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def planets(self):
+        with create_connection(self.args) as db:
+            cur = db.cursor()
+            cur.execute("SELECT id, name FROM Planet P")
+            result = []
+            planets = cur.fetchall()
+            for p in planets:
+                result.append({"id": p[0], "name": p[1]})
+            return result
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def sportsman(self):
+        db = connection_factory.getconn()
+        try:
+          cur = db.cursor()
+          cur.execute("SELECT id, name, country_id, volunteer_id  FROM Sportsmens")
+          result = []
+          commanders = cur.fetchall()
+          for c in commanders:
+              result.append({"id": c[0], "name": c[1], "counry_id":c[2], "volunteer_id":c[3]})
+          return result
+        finally:
+          connection_factory.putconn(db)
+  
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def countries(self):
+        db = connection_factory.getconn()
+        try:
+            cur = db.cursor()
+            cur.execute("SELECT * FROM Countries;")
+            result = []
+            commanders = cur.fetchall()
+            for c in commanders:
+                result.append({"id": c[0], "name": c[1]})
+            return result
+        finally:
+          connection_factory.putconn(db)
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def volunteers(self):
+        db = connection_factory.getconn()
+        try:
+        # with create_connection(self.args) as db:
+            cur = db.cursor()
+            cur.execute("SELECT id, name FROM Volunteers;")
+            result = []
+            commanders = cur.fetchall()
+            for c in commanders:
+                result.append({"id": c[0], "name": c[1]})
+            return result
+        finally:
+          connection_factory.putconn(db)
+
+    @cherrypy.expose
+
+    def register(self, sportsman, country, volunteer_id):
+        status_message = 'SUCCESS'
+        db = connection_factory.getconn()
+        try:
+          cur = db.cursor()
+          cur.execute("SELECT id FROM Countries WHERE Countries.name=%s;", (country,))
+          country_id, volunteer_id = int(cur.fetchone()[0]), int(volunteer_id)
+          if sportsman.isdigit():
+            sportsman_id = int(sportsman)
+            cur.execute("UPDATE Sportsmens SET country_id=%s, volunteer_id=%s WHERE id=%s;", (country_id, volunteer_id, sportsman_id))
+          else:
+            cur.execute("INSERT INTO Sportsmens(name, gender, country_id, object_id, volunteer_id) VALUES(%s, %s, %s, 1, %s);", (sportsman, 'm', country_id, volunteer_id))  
+        except Exception as e:
+          status_message = f"Error: {e}"
+        finally:
+          connection_factory.putconn(db)
+        return status_message
+
+
+def run():
+    cherrypy_cors.install()    
+    cherrypy.config.update({
+      'server.socket_host': '0.0.0.0',
+      'server.socket_port': 8080,
+    })
+    config = {
+        '/': {
+            'cors.expose.on': True,
+        },
+    }
+    cherrypy.quickstart(App(parse_cmd_line()), config=config)
+
+if __name__ == '__main__':
+  run()
--- project16/connect.py
+++ project16/connect.py
@@ -6,6 +6,8 @@ import argparse
 # Находится в модуле psycopg2-binary, который можно установить командой
 # pip install psycopg2-binary или её аналогом.
 import psycopg2 as pg_driver
+from psycopg2 import pool
+
 # Драйвер SQLite3
 # Находится в модуле sqlite3, который можно установить командой
 # pip install sqlite3 или её аналогом.
@@ -47,3 +49,46 @@ def create_connection(args):
 
 
 
+class ConnectionFactory:
+    def __init__(self, open_fxn, close_fxn):
+        self.open_fxn = open_fxn
+        self.close_fxn = close_fxn
+
+    def getconn(self):
+        return self.open_fxn()
+
+    def putconn(self, conn):
+        return self.close_fxn(conn)
+
+
+def create_connection_factory(args):
+    # Создаёт подключение к SQLite в соответствии с аргументами командной строки.
+    def open_sqlite():
+        return sqlite_driver.connect(args.sqlite_file)
+
+    def close_sqlite(conn):
+        pass
+
+    # Создаёт подключение в соответствии с аргументами командной строки.
+    # Если указан аргумент --sqlite-file то создается подключение к SQLite,
+    # в противном случае создаётся подключение к PostgreSQL
+    if args.sqlite_file is not None:
+        return ConnectionFactory(open_fxn=open_sqlite, close_fxn=close_sqlite)
+    else:
+        # Создаёт подключение к постгресу в соответствии с аргументами командной строки.
+        pg_pool = pool.SimpleConnectionPool(1, 5,
+                                  user=args.pg_user, password=args.pg_password, host=args.pg_host, port=args.pg_port)
+        count = 0
+        def open_pg():
+            nonlocal count
+            count += 1
+            print(f"We issued {count} queries")
+            return pg_pool.getconn()
+
+        def close_pg(conn):
+            pg_pool.putconn(conn)
+
+        return ConnectionFactory(open_fxn=open_pg, close_fxn=close_pg)
+
+
+connection_factory = create_connection_factory(parse_cmd_line())
