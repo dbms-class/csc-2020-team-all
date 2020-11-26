2d683729ce540dc0871d2b92bb403b8020901491
repl.it user <replituser@example.com>
2020-11-26 17:19:58 +0000

    26_11 done

--- project7/connect.py
+++ project7/connect.py
@@ -4,13 +4,13 @@ import argparse
 import psycopg2 as pg_driver
 
 def parse_cmd_line():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pg-host', help='PostgreSQL host name', default='rogue.db.elephantsql.com')
-    parser.add_argument('--pg-port', help='PostgreSQL port', default=5432)
-    parser.add_argument('--pg-user', help='PostgreSQL user', default='lfiykhno')
-    parser.add_argument('--pg-password', help='PostgreSQL password', default='NVm2RUHJXAq8CMQ8ZGrAbzTww1orBUIP')
-    parser.add_argument('--pg-database', help='PostgreSQL database', default='bfzentwe')
-    return parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--pg-host', help='PostgreSQL host name', default='rogue.db.elephantsql.com')
+  parser.add_argument('--pg-port', help='PostgreSQL port', default=5432)
+  parser.add_argument('--pg-user', help='PostgreSQL user', default='lfiykhno')
+  parser.add_argument('--pg-password', help='PostgreSQL password', default='NVm2RUHJXAq8CMQ8ZGrAbzTww1orBUIP')
+  parser.add_argument('--pg-database', help='PostgreSQL database', default='bfzentwe')
+  return parser.parse_args()
 
 
 # Создаёт подключение к постгресу в соответствии с аргументами командной строки.
