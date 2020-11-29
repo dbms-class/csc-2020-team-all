0a90d6388f9ccabf107129812eab03666eefae19
Artem Ioselevskii <artem.ioselevskii@gmail.com>
2020-11-29 18:11:12 +0300

    project12: use session pool from peewee

--- project12/upload_db.py
+++ project12/upload_db.py
@@ -1,17 +1,21 @@
-from connect import create_connection
+from connect import create_connection_factory
 from connect import parse_cmd_line
 
 
 class Uploader(object):
     def __init__(self, args):
-        self.args = args
+        self.connection_factory = create_connection_factory(args)
 
     def run_sql_script(self, filename):
         sql_file = open(filename)
         sql_as_string = sql_file.read()
-        with create_connection(self.args) as db:
+        db = self.connection_factory.getconn()
+        try:
             cur = db.cursor()
             cur.execute(sql_as_string)
+            db.commit()
+        finally:
+            self.connection_factory.putconn(db)
 
 
 if __name__ == '__main__':
--- project12/webapp.py
+++ project12/webapp.py
@@ -1,15 +1,15 @@
 ## Веб сервер
 import cherrypy
 
+from connect import create_connection_factory
 from connect import parse_cmd_line
-from connect import create_connection
 from static import index
 
 
 @cherrypy.expose
 class App(object):
     def __init__(self, args):
-        self.args = args
+        self.connection_factory = create_connection_factory(args)
 
     @cherrypy.expose
     def start(self):
@@ -22,7 +22,8 @@ class App(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def update_retail(self, drug_id, pharmacy_id, remainder, price):
-        with create_connection(self.args) as db:
+        db = self.connection_factory.getconn()
+        try:
             cur = db.cursor()
             query = """
                 UPDATE Availability
@@ -37,12 +38,16 @@ class App(object):
                 )
             """
             cur.execute(query, {'drug_id': drug_id, 'pharmacy_id': pharmacy_id, 'remainder': remainder, 'price': price})
+            db.commit()
             return {"status": "OK"}
+        finally:
+            self.connection_factory.putconn(db)
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def drugs(self):
-        with create_connection(self.args) as db:
+        db = self.connection_factory.getconn()
+        try:
             cur = db.cursor()
             query = """
                 SELECT M.id, M.trade_name, A.title as inn
@@ -55,11 +60,14 @@ class App(object):
             for m in medicines:
                 result.append({"id": m[0], "trade_name": m[1], "inn": m[2]})
             return result
+        finally:
+            self.connection_factory.putconn(db)
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def pharmacies(self):
-        with create_connection(self.args) as db:
+        db = self.connection_factory.getconn()
+        try:
             cur = db.cursor()
             query = """
                 SELECT id, title num, address
@@ -71,6 +79,8 @@ class App(object):
             for m in medicines:
                 result.append({"id": m[0], "num": m[1], "address": m[2]})
             return result
+        finally:
+            self.connection_factory.putconn(db)
 
 
 cherrypy.config.update({
