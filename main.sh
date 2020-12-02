197b8e277a77893d23bf45b2981ab04911961c93
Artem Ioselevskii <artem.ioselevskii@gmail.com>
2020-12-02 04:20:00 +0300

    project12: add /status_retail

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
-#!bin/sh
+##!bin/sh
 #pip install cherrypy psycopg2-binary
 
 PG_HOST=hattie.db.elephantsql.com
 PG_PORT=5432
 PG_USER=ujrazvgd
 PG_DATABASE=ujrazvgd
-PG_PASSWORD=<password>
+PG_PASSWORD=WVHDSjU8ZURw6S-S7gc4Z5Ij1EoNQP8t
 
 # Upload schemes and test data to db. Usage example:
-# cd project12 && python upload_db.py --pg-host $PG_HOST --pg-port $PG_PORT --pg-user $PG_USER --pg-password $PG_PASSWORD --pg-database $PG_DATABASE
+#cd project12 && python3 upload_db.py --pg-host $PG_HOST --pg-port $PG_PORT --pg-user $PG_USER --pg-password $PG_PASSWORD --pg-database $PG_DATABASE
 
 # App. Usage example:
-cd project12 && python webapp.py --pg-host $PG_HOST --pg-port $PG_PORT --pg-user $PG_USER --pg-password $PG_PASSWORD --pg-database $PG_DATABASE
+cd project12 && python3 webapp.py --pg-host $PG_HOST --pg-port $PG_PORT --pg-user $PG_USER --pg-password $PG_PASSWORD --pg-database $PG_DATABASE
--- project12/webapp.py
+++ project12/webapp.py
@@ -16,7 +16,7 @@ class App(object):
 
     @cherrypy.expose
     def start(self):
-        return "Hello web app"
+        return 'Hello web app'
 
     @cherrypy.expose
     def index(self):
@@ -106,7 +106,7 @@ class App(object):
             result = []
             medicines = cur.fetchall()
             for m in medicines:
-                result.append({"id": m[0], "trade_name": m[1], "inn": m[2]})
+                result.append({'id': m[0], 'trade_name': m[1], 'inn': m[2]})
             return result
 
     @cherrypy.expose
@@ -122,8 +122,53 @@ class App(object):
             result = []
             medicines = cur.fetchall()
             for m in medicines:
-                result.append({"id": m[0], "num": m[1], "address": m[2]})
+                result.append({'id': m[0], 'num': m[1], 'address': m[2]})
             return result
+
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def status_retail(self, drug_id=None, min_remainder=None, max_price=None):
+        with self.connection_factory.conn() as db:
+            availability = Table('availability').bind(db)
+            medicine = Table('medicine').bind(db)
+            pharmacy = Table('pharmacy').bind(db)
+
+            min_max_price = availability.select(
+                availability.c.medicine_id,
+                fn.MIN(availability.c.price),
+                fn.MAX(availability.c.price)
+            ).group_by(availability.c.medicine_id)
+
+            query = availability.select(
+                availability.c.medicine_id.alias('drug_id'),
+                medicine.c.trade_name.alias('drug_trade_name'),
+                medicine.c.international_trade_name.alias('drug_inn'),
+                availability.c.pharmacy_id.alias('pharmacy_id'),
+                pharmacy.c.address.alias('pharmacy_address'),
+                availability.c.remainder.alias('remainder'),
+                availability.c.price.alias('price'),
+                min_max_price.c.min.alias('min_price'),
+                min_max_price.c.max.alias('max_price')
+            ).join(
+                medicine, on=(availability.c.medicine_id == medicine.c.id)
+            ).join(
+                pharmacy, on=(availability.c.pharmacy_id == pharmacy.c.id)
+            ).join(
+                min_max_price, on=(availability.c.medicine_id == min_max_price.c.medicine_id)
+            )
+
+            if drug_id is not None:
+                query = query.where(availability.c.medicine_id == drug_id)
+
+            if min_remainder is not None:
+                query = query.where(availability.c.remainder >= min_remainder)
+
+            if max_price is not None:
+                query = query.where(availability.c.price <= max_price)
+
+            stocks = list(query.execute())
+            return stocks
 
 
 cherrypy.config.update({
