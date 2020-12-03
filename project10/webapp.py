b791b4b8147e7b0b2fe287d2d0b2b1cf1892a0ba
Scherbakov, Maksim <maksim.scherbakov@wartsila.com>
2020-12-03 21:00:05 +0300

    shift_timetable handler

--- project10/webapp.py
+++ project10/webapp.py
@@ -76,7 +76,7 @@ class App(object):
         try:
             cur = db.cursor()
 
-            if (route_id):
+            if route_id:
                 cur.execute("SELECT * FROM TimetableStopView WHERE stop_id=%s AND route_id=%s AND is_weekend=%s",
                             (stop_id, route_id, bool(is_working_day)))
             else:
@@ -96,6 +96,37 @@ class App(object):
                 for g in cur.fetchall():
                     for r in result:
                         r.update(all_first_arrival=g[1], all_last_arrival=g[2])
+        finally:
+            connection_factory.putconn(db)
+        return result
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def shift_timetable(self, route_num, stop_ids, shift_min, is_working_day, start_min=0, end_min=1440):
+        db = connection_factory.getconn()
+        try:
+            cur = db.cursor()
+            timetable = []
+            cur.execute(
+                "SELECT stop_id, platform_number, address, time_min FROM timetable T JOIN stop S ON T.stop_id=S.id WHERE route_id=%s and is_weekend=%s",
+                (route_num, is_working_day))
+            if stop_ids == '*':
+                for spat in cur.fetchall():
+                    stop, platform, address, time = spat
+                    timetable.append((stop, platform, address, time))
+            else:
+                sp = stop_ids.split(',')
+                for e in sp:
+                    stop, platform = e.split('-')
+                    for el in cur.fetchall():
+                        if el[0] == stop and el[1] == platform:
+                            timetable.append((stop, platform, el[2], el[3]))
+            result = [
+                {"stop_id": g[0], "stop_name": g[1], "platform": g[2], "old_time": g[3],
+                 "new_time": str(int(g[3]) + int(shift_min))
+                 }
+                for g in timetable
+            ]
         finally:
             connection_factory.putconn(db)
         return result
