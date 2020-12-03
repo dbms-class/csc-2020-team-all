f6568499b5ced166575187a77f554fc67e266507
Alexander Marinskiy <marinskiy@gmail.com>
2020-12-03 20:57:19 +0300

    03122020 progress

--- project11/app.py
+++ project11/app.py
@@ -9,6 +9,7 @@ from connect import parse_cmd_line
 from connect import create_connection, connection_factory
 from static import index
 
+
 @cherrypy.expose
 class App(object):
     def __init__(self, args):
@@ -20,12 +21,13 @@ class App(object):
 
     @cherrypy.expose
     def index(self):
-      return index()
-  
+        return index()
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def countries(self):
-        with create_connection(self.args) as db:
+        db = connection_factory.getconn()
+        try:
             cur = db.cursor()
             cur.execute("SELECT * FROM Countries;")
             result = []
@@ -33,11 +35,14 @@ class App(object):
             for c in commanders:
                 result.append({"id": c[0], "name": c[1]})
             return result
+        finally:
+            connection_factory.putconn(db)
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def volunteers(self):
-        with create_connection(self.args) as db:
+        db = connection_factory.getconn()
+        try:
             cur = db.cursor()
             cur.execute("SELECT id, name FROM Volunteers;")
             result = []
@@ -45,58 +50,125 @@ class App(object):
             for c in commanders:
                 result.append({"id": c[0], "name": c[1]})
             return result
+        finally:
+            connection_factory.putconn(db)
 
     @cherrypy.expose
     def register(self, sportsman, country, volunteer_id):
         status_message = 'SUCCESS'
-        with create_connection(self.args) as db:
-          # db = connection_factory.getconn()
-          try:
+        db = connection_factory.getconn()
+        try:
             cur = db.cursor()
             cur.execute("SELECT id FROM Countries WHERE Countries.name=%s;", (country,))
             country_id = int(cur.fetchone()[0])
             volunteer_id = int(volunteer_id)
             if sportsman.isdigit():
-              sportsman_id = int(sportsman)
-              cur.execute("UPDATE sportsman SET country_id=%s, volunteer_id=%s WHERE id=%s;", (country_id, volunteer_id, sportsman_id))
-              status_message = 'UPDATED'
+                sportsman_id = int(sportsman)
+                cur.execute("UPDATE sportsman SET country_id=%s, volunteer_id=%s WHERE id=%s;",
+                            (country_id, volunteer_id, sportsman_id))
+                status_message = 'UPDATED'
             else:
-              cur.execute("INSERT INTO sportsman (name, country_id, volunteer_id) VALUES(%s, %s, %s);", (sportsman, country_id, volunteer_id))
-              status_message = 'INSERTED'
-          finally:
-            pass
-            # connection_factory.putconn(db)
-        return status_message
+                cur.execute("INSERT INTO sportsman (name, country_id, volunteer_id) VALUES(%s, %s, %s);",
+                            (sportsman, country_id, volunteer_id))
+                status_message = 'INSERTED'
+                return status_message
+        finally:
+            connection_factory.putconn(db)
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def volunteer_load(self, volunteer_id=, sportsman_count, total_task_count):
-      pass
-    
-      # query = []
-      # if (volunteer_id):
-      #   query.append("volunteer_id=%s")
-      #   Valunteer -> Sportsmens
-      #   Valunterr -> Task
-      #   Aggr(first, second):
-      #   [
-      #     {
-      #     ”volunteer_id”: 1,
-      #     ”volunteer_name”: ”Pedro”,
-      #     ”sportsman_count”: 20,
-      #     ”total_task_count”: 3, // общее количество задач
-      #     ”next_task_id”: 146, // id ближайшей задачи к now
-      #     ”next_task_time”: ”2020-11-19 12:00” // время ближайшей задачи
-      #     },
-      #     ...
-      #   ]
+    def volunteer_load(self, volunteer_id=None, sportsman_count=0, total_task_count=0):
+        db = connection_factory.getconn()
+        sportsman_count = int(sportsman_count)
+        total_task_count = int(total_task_count)
+        try:
+            cur = db.cursor()
+            if volunteer_id is None:
+                cur.execute(
+                    "SELECT * FROM VolunteerLoad WHERE VolunteerLoad.sportsman_count >= %s AND VolunteerLoad.total_task_count>=%s;",
+                    (sportsman_count, total_task_count))
+            else:
+                volunteer_id = int(volunteer_id)
+                cur.execute(
+                    "SELECT * FROM VolunteerLoad WHERE VolunteerLoad.sportsman_count >= %s AND VolunteerLoad.total_task_count>=%s AND VolunteerLoad.volunteer_id = %s;",
+                    (sportsman_count, total_task_count, volunteer_id))
+            result = []
+            for f in cur.fetchall():
+                result.append({
+                    "volunteer_id": f[0],
+                    "volunteer_name": f[1],
+                    "sportsman_count": f[2],
+                    "total_task_count": f[3],
+                    "next_task_time": "" if f[4] is None else f[4].strftime("%m/%d/%Y, %H:%M:%S"),
+                    "next_task_id": f[5]
+                })
+            return result
+        finally:
+            connection_factory.putconn(db)
 
 
+# def volunteer_unassign(self, volunteer_id, task_ids):
+#   db = connection_factory.getconn()
+#   volunteer_id = int(volunteer_id)
+#   try:
+#     cur = db.cursor()
+#     cur.execute("SELECT id,name FROM volunteers WHERE volunteers.id=%s",(volunteer_id)
+#     quitter = Volunteer(cur.fetchone())
+#     for task in quitter.tasks():
+#       cur = db.cursor()
+#       cur.execute("SELECT id,name FROM volunteers WHERE volunteers.id=%s",(volunteer_id)
+  # if task_ids = '*':
+
+  # else:
+
+
+class Volunteer:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def delegations(self):
+        db = connection_factory.getconn()
+        try:
+            cur = db.cursor()
+            cur.execute(
+                "SELECT DISTINCT delegation_id FROM sportsman WHERE volunteer_id=%s", (self.id,)
+            )
+            result = set()
+            for f in cur.fetchall():
+                result.add(tas[0])
+            return result
+        finally:
+            connection_factory.putconn(db)
+
+     def tasks(self):
+        db = connection_factory.getconn()
+        try:
+            cur = db.cursor()
+            cur.execute(
+                "SELECT id, date, time FROM volunteer_task WHERE volunteer_id=%s", (self.id,)
+            )
+            result = set()
+            for f in cur.fetchall():
+                result.add(Task(f[0], f[1], f[2]))
+            return result
+        finally:
+            connection_factory.putconn(db)
+
+
+def Task(self):
+  def __init__(self, id, _date, _time):
+    self.id = id
+    self.date = _date
+    self.time = _time
+       
+
+# volunteer_task,id, date, time
 def run():
-    cherrypy_cors.install()    
+    cherrypy_cors.install()
     cherrypy.config.update({
-      'server.socket_host': '0.0.0.0',
-      'server.socket_port': 8080,
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': 8080,
     })
     config = {
         '/': {
@@ -105,5 +177,6 @@ def run():
     }
     cherrypy.quickstart(App(parse_cmd_line()), config=config)
 
+
 if __name__ == '__main__':
-  run()
+    run()
