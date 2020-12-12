f9f9080c73ff53375c17f1069984bad1d1cbeb93
Dmitry Barashev <dmitry@barashev.net>
2020-12-12 00:34:31 +0000

    work 2020-12-10

--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
-pip install cherrypy psycopg2-binary cherrypy_cors
+#pip install cherrypy psycopg2-binary cherrypy_cors
+#pip install peewee
+#pip install playhouse
 python3 ./project16/app.py --pg-user swrsmqyz --pg-password UvRefGcdpZ5oVONlXsQBU6Ir_WZi45Kr --pg-database swrsmqyz --pg-host rogue.db.elephantsql.com --pg-port 5432
